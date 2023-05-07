254693f757ffe55d4d7fdbec9e86a5b98aaea033
MarisaSeg <mesthor683@gmail.com>
2023-05-07 14:21:11 -0300

    Subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function App() {
           manejarClick={reiniciarContador}
         />
         <Boton 
-          oton 
           texto='Click'
           esBotonDeClick={true}
           manejarClick={manejarClick}
